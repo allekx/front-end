d37a30d30ffc3f947e08883415bf8a108141f7c4
allekx <alexsander9410@gmail.com>
2024-10-27 23:05:26 -0400

    Botões prontos e funcionais

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function toggleActive() {
   const image = document.getElementById("boneco");
-  image.classList.toggle("active"); // Alterna a classe "active" na imagem
+  image.classList.toggle("active"); 
 
   const tirar = document.getElementById("aranha");
   tirar.classList.toggle("active");
+
+  const tirarButton = document.getElementById("esquerda");
+  tirarButton.classList.toggle("active");
+
+  const sumirButton = document.getElementById("direita");
+  sumirButton.classList.toggle("active");
 }
